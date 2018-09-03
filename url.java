51e26092fa3a9490ae2b51feebbca5f912a627ec
Abdi Mohamud <amohamud23@gmail.com>
2018-09-03 00:03:05 -0700

    created a stack to push the characters into

--- url.java
+++ url.java
@@ -1,12 +1,37 @@
 import java.net.*;
 import java.io.*;
+import java.lang.*;
+import java.util.Stack;
 
 public class url   {
     public static void main(String[] args)  throws Exception{
-        
+         int j =0;
+         int counter = 0;
         URL aUrl = new URL("http://www.mail.yahoo.com");
+        String str = aUrl.getHost();
 
-        System.out.println(aUrl.getHost()); 
+        System.out.println(str); 
+
+        for (int i = 0; i < str.length(); i++)
+            {
+                  if (str.charAt(i) == '.')
+                    {
+                      j = i-1;  
+                      
+                    }
+                    
+
+            }
+                Stack<Character> stack = new Stack<Character>();
+        while (str.charAt(j) != '.')
+            {
+                stack.push(str.charAt(j));
+                counter++;
+                j--;
+            }
+
+            System.out.println(stack);
+       
 
     }
 
